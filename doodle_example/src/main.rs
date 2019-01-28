01818001c8f2434604b57706a36e7c5544560d0a
Daniel Kiss <littlesnorrboy@gmail.com>
2019-01-28 09:28:50 +0100

    Add initial travis.yml

--- doodle_example/src/main.rs
+++ doodle_example/src/main.rs
@@ -1,4 +1,5 @@
 extern crate doodle;
+extern crate serde_json;
 
 use doodle::*;
 
@@ -15,6 +16,7 @@ trait SomeTrait {}
 
 fn main() {
     let fields = Foo::get_fields_openapi();
-    println!("\"Foo\" Schema: \n{:#?}", fields);
+    let output = serde_json::to_string_pretty(&fields).unwrap();
+    println!("\"Foo\" Schema: \n{}", output);
 }
 
